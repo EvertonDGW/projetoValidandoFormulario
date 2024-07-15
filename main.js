8ce2dc61d70d6116198644d84fc3a073e8ebd0d0
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2024-07-14 21:30:32 -0300

    atualizado

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class ValidaFormulario {
         });
     }
 
-    handleSubmit(e) {
+    handleSubmit() {
         e.preventDefault();
         const camposValidos = this.camposSaoValidos();
         const senhasValidas = this.senhasSaoValidas();
